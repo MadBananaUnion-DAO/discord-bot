1e311327705bad1f8b0a622417c0484d11bef1e4
Kai-ONeal <kai.oneal07@gmail.com>
2024-07-08 09:50:23 -0500

    TEST: created  a test slash command , button component, & interaction handler

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -1,7 +1,16 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { CreateDiscordDto } from './dto/create-discord.dto';
 import { UpdateDiscordDto } from './dto/update-discord.dto';
-import { Client, GatewayIntentBits } from 'discord.js';
+import {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  Interaction,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} from 'discord.js';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -17,6 +26,7 @@ export class DiscordService implements OnModuleInit, OnModuleDestroy {
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
       ],
     });
     this.token = this.configService.getOrThrow<string>('MY_TEST_BOT_TOKEN');
@@ -29,14 +39,14 @@ export class DiscordService implements OnModuleInit, OnModuleDestroy {
       console.log(`Logged in as ${this.client.user.tag}!`);
 
       // Register slash commands on bot startup
-      // await this.registerSlashCommands();
+      await this.registerSlashCommands();
     });
 
     this.client.on('interactionCreate', async (interaction) => {
       if (!interaction.isCommand()) return;
 
       // const command = interaction.commandName;
-      // await this.handleSlashCommand(command, interaction);
+      await this.handleInteraction(interaction);
     });
 
     await this.client.login(this.token);
@@ -51,51 +61,105 @@ export class DiscordService implements OnModuleInit, OnModuleDestroy {
     }
   }
 
-  // async registerSlashCommands() {
-  //   const rest = new REST({ version: '9' }).setToken(this.token);
+  async registerSlashCommands() {
+    const rest = new REST({ version: '9' }).setToken(this.token);
 
-  //   try {
-  //     console.log('Started refreshing application (/) commands.');
+    try {
+      console.log('Started refreshing application (/) commands.');
 
-  //     // Replace with your Discord application client ID and guild ID (if applicable)
-  //     const clientId = this.client.user.id;
+      // Replace with your Discord application client ID and guild ID (if applicable)
+      const clientId = this.client.user.id;
 
-  //     const commands = [
-  //       {
-  //         name: 'ping',
-  //         description: 'Replies with Pong!',
-  //       },
-  //       // Add more commands as needed
-  //     ];
+      const commands = [
+        {
+          name: 'ping',
+          description: 'Replies with Pong!',
+        },
+        {
+          name: 'button',
+          description: 'Replies with Pong!',
+        },
+        // Add more commands as needed
+      ];
 
-  //     if (this.guildId) {
-  //       await rest.put(
-  //         Routes.applicationGuildCommands(clientId, this.guildId),
-  //         { body: commands },
-  //       );
-  //     } else {
-  //       await rest.put(Routes.applicationCommands(clientId), {
-  //         body: commands,
-  //       });
-  //     }
+      if (this.guildId) {
+        await rest.put(
+          Routes.applicationGuildCommands(clientId, this.guildId),
+          { body: commands },
+        );
+      } else {
+        await rest.put(Routes.applicationCommands(clientId), {
+          body: commands,
+        });
+      }
 
-  //     console.log('Successfully registered application (/) commands.');
-  //   } catch (error) {
-  //     console.error('Error refreshing application (/) commands:', error);
-  //   }
-  // }
+      console.log('Successfully registered application (/) commands.');
+    } catch (error) {
+      console.error('Error refreshing application (/) commands:', error);
+    }
+  }
 
-  // async handleSlashCommand(command: string, interaction: any) {
-  //   switch (command) {
-  //     case 'ping':
-  //       await interaction.reply('Pong!');
-  //       break;
-  //     // Handle other commands similarly
-  //     default:
-  //       await interaction.reply('Command not recognized.');
-  //       break;
-  //   }
-  // }
+  async handleInteraction(interaction: Interaction) {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    switch (commandName) {
+      case 'ping':
+        await interaction.reply('Pong!');
+        break;
+      case 'button':
+        const confirm = new ButtonBuilder()
+          .setCustomId('confirm')
+          .setLabel('Confirm Ban')
+          .setStyle(ButtonStyle.Success);
+
+        const cancel = new ButtonBuilder()
+          .setCustomId('cancel')
+          .setLabel('Cancel')
+          .setStyle(ButtonStyle.Secondary);
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+          ...[confirm, cancel],
+        );
+
+        const response = await interaction.reply({
+          content: 'Please confirm or cancel the action.',
+          components: [row],
+        });
+
+        const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+        try {
+          const confirmation = await response.awaitMessageComponent({
+            filter: collectorFilter,
+            time: 10_000,
+          });
+
+          if (confirmation.customId === 'confirm') {
+            // await interaction.guild.members.ban(target);
+            await confirmation.update({
+              content: `You have just hit the confirm button`,
+              components: [],
+            });
+          } else if (confirmation.customId === 'cancel') {
+            await confirmation.update({
+              content: `You have just hit the Cancel buton button`,
+              components: [],
+            });
+          }
+        } catch (e) {
+          await interaction.editReply({
+            content: 'Confirmation not received within 1 minute, cancelling',
+            components: [],
+          });
+        }
+        break;
+      default:
+        await interaction.reply('Command not recognized.');
+        break;
+    }
+  }
 
   create(createDiscordDto: CreateDiscordDto) {
     return `This action adds a new discord : ${createDiscordDto}`;
