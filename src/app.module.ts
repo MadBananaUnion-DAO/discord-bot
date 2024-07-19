6e1642d2940a608d7c384c275eb995e91873fcee
MonkeyMike <108690228+mon-key-mike@users.noreply.github.com>
2024-07-18 22:48:17 -0400

    Update app.module.ts
    Signed-off-by: MonkeyMike <108690228+mon-key-mike@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DiscordModule } from './discord/discord.module';
 import { ConfigModule } from '@nestjs/config';
+import { DiscordModule } from './discord/discord.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DiscordModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DiscordModule,
+  ],
 })
 export class AppModule {}
