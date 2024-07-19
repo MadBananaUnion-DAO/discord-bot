086c15cf7e92067f8cf92b116b93545bd93ad468
MonkeyMike <108690228+mon-key-mike@users.noreply.github.com>
2024-07-18 22:46:55 -0400

    Update discord.module.ts
    Signed-off-by: MonkeyMike <108690228+mon-key-mike@users.noreply.github.com>

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DiscordService } from './discord.service';
-import { DiscordController } from './discord.controller';
 
 @Module({
-  controllers: [DiscordController],
+  imports: [ConfigModule.forRoot()],
   providers: [DiscordService],
+  exports: [DiscordService],
 })
 export class DiscordModule {}
